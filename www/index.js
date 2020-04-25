4f1959f564bac2fa4cbc7e8dcfeecc0b07211a5a
Diamondlord <vladimir.almazov@gmail.com>
2020-04-25 19:46:25 +0300

    implement game of life

--- www/index.js
+++ www/index.js
@@ -1,3 +1,11 @@
-import * as wasm from "wasm-game-of-life";
-const name = "Vova";
-wasm.greet(name);
+import { Universe } from "wasm-game-of-life";
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  // requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
